f0f63f5f64af223f40a916402ed1ba5c0192d41f
EduardoFertigBastos <dudufbastos1@gmail.com>
2020-10-06 21:38:05 -0300

    Exe01

---
+++ 10_06/exe01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exe 01</title>
+</head>
+<body>
+    <?php
+    $x = 1;
+    while($x < 51) {
+        echo 'Linha ' . $x . ' ........................................ <br />';
+        $x = $x + 1;
+    }
+    ?>
+</body>
+</html>
