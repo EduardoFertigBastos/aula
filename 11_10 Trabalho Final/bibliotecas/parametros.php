adf3f51f17940c98eb91159a90dcd1127fb3fcaa
EduardoFertigBastos <dudufbastos1@gmail.com>
2020-11-11 11:11:06 -0300

    Criação dos Cadastros e organização dos parâmetros

--- 11_10 Trabalho Final/bibliotecas/parametros.php
+++ 11_10 Trabalho Final/bibliotecas/parametros.php
@@ -1,18 +1,20 @@
 <?php
     define('ROOT_DIR',                  dirname(__DIR__));
+
     define('BIBLIOTECAS',               dirname(__DIR__) . '/bibliotecas/');
-    define('LAYOUTS',                   dirname(__DIR__) . '/layouts/');
     define('CADASTROS',                 dirname(__DIR__) . '/cadastros/');
-    define('PRODUTOS',                  dirname(__DIR__) . '/cadastros/produtos/');
+    define('LAYOUTS',                   dirname(__DIR__) . '/layouts/');
+
     define('CATEGORIAS',                dirname(__DIR__) . '/cadastros/categorias/');
     define('CLIENTES',                  dirname(__DIR__) . '/cadastros/clientes/');
-    define('FORNECEDORES',              dirname(__DIR__) . '/cadastros/fornecedores/');
-    define('TERRITORIO',                dirname(__DIR__) . '/cadastros/territorios/');
-    define('REGIAO',                    dirname(__DIR__) . '/cadastros/regiao/');
-    define('ORDENS',                    dirname(__DIR__) . '/cadastros/ordens/');
-    define('FUNCIONARIOS',              dirname(__DIR__) . '/cadastros/funcionarios/');
-    define('TRANSPORTADORAS',           dirname(__DIR__) . '/cadastros/transportadoras/');
     define('CLIENTESDEMOGRAFICOS',      dirname(__DIR__) . '/cadastros/clientesDemograficos/');
     define('CUSTOMERCUSTOMERDEMO',      dirname(__DIR__) . '/cadastros/customerCustomerDemo/');
+    define('FORNECEDORES',              dirname(__DIR__) . '/cadastros/fornecedores/');
+    define('FUNCIONARIOS',              dirname(__DIR__) . '/cadastros/funcionarios/');
     define('FUNCIONARIOSTERRITORIOS',   dirname(__DIR__) . '/cadastros/funcionarioTerritorios/');
+    define('ORDENS',                    dirname(__DIR__) . '/cadastros/ordens/');
     define('ORDENSDETALHES',            dirname(__DIR__) . '/cadastros/ordensDetalhes/');
+    define('PRODUTOS',                  dirname(__DIR__) . '/cadastros/produtos/');
+    define('REGIAO',                    dirname(__DIR__) . '/cadastros/regiao/');
+    define('TERRITORIO',                dirname(__DIR__) . '/cadastros/territorios/');
+    define('TRANSPORTADORAS',           dirname(__DIR__) . '/cadastros/transportadoras/');
