1dbf7dc7e5acfbad3c4d06e80415de7c9c780089
EduardoFertigBastos <dudufbastos1@gmail.com>
2020-11-11 11:52:03 -0300

    Index

---
+++ 11_10 Trabalho Final/layouts/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once LAYOUTS . 'head.php';
+require_once LAYOUTS . 'menu.php';
+
+if (!isset($_GET['pagina'])) {
+    require_once 'home.php';
+} else {
+    require_once 'cadastros/' . $_GET['modulo'] . '/' . $_GET['pagina'] . '.php';
+}
+
+require_once LAYOUTS . 'footer.php';
