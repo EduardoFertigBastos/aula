5dab3db3f8a85608111f6a9524ae9ddc2e20c410
EduardoFertigBastos <dudufbastos1@gmail.com>
2020-11-15 23:25:42 -0300

    Deletar transportadora

---
+++ 11_10 Trabalho Final/cadastros/transportadoras/deletar.php
@@ -0,0 +1,42 @@
+<?php
+function confirmacaoDeletar($id) 
+{
+    echo '<form method="post" action="' . LOCALHOST .'?pg=transportadoras&deletar=' . $id . '">
+
+            Deseja realmente deletar o Registro #'. $id . '?
+       
+            <input type="submit" value="Deletar" name="deletar" class="btn btn-primary col-sm-8 col-md-10 col-lg-8 py-2">
+            
+        </form>'; 
+}
+
+function deletarBanco($conn)
+{
+    try {
+        if (isset($_POST['deletar'])) {
+            /**
+             * Deletar no Banco de Dados o registro.
+             */
+            $sSql = "DELETE
+                       FROM transportadoras
+                      WHERE IDTransportadora = :id";
+         
+            $stmt = $conn->prepare($sSql);
+            
+            $stmt->bindParam(':id', $_GET['deletar'], PDO::PARAM_INT);
+            $stmt->execute();
+            
+            /**
+             * Redirecionar para a listagem.
+             */
+            redirecionar(
+                'transportadoras', 
+                'Registro deletado com sucesso.'
+            );
+        }
+    } catch(PDOException $e) {
+        imprimirErro($e);
+    }
+}
+
+deletarBanco($conn);
