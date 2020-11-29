66e7562b939c5e053f36b68551d16a5775169f1f
EduardoFertigBastos <dudufbastos1@gmail.com>
2020-11-28 21:53:40 -0300

    Alteração Menu

--- 11_10 Trabalho Final/layouts/menu.php
+++ 11_10 Trabalho Final/layouts/menu.php
@@ -1,42 +1,27 @@
 <?php
 
-    
-    /**
-     * Pegar as tabelas sem '_' no meio do título 
-     */
-    $sSql = "SHOW TABLES
-             FROM trabalhofinal
-            WHERE Tables_in_trabalhofinal NOT LIKE '%\_%';";
-
-    $stmt = $conn->prepare($sSql);
-    $stmt->execute();
-
-    $aItensSoltos = $stmt->fetchAll();
-
-    /**
-     * Pegar as tabelas com '_' no meio do título 
-     */
-    $sSql = "SHOW TABLES
-             FROM trabalhofinal
-            WHERE Tables_in_trabalhofinal LIKE '%\_%';";
-
-    $stmt = $conn->prepare($sSql);
-    $stmt->execute();
-
-    $aItensDropdown = $stmt->fetchAll();
-
-    /**
-     * Diminuir os itens principais do menu
-     */
-    function mudarMenus(&$aItensSoltos, &$aItensDropdown, $iTamanho) 
-    {
-        for ($x = sizeof($aItensSoltos); $x > $iTamanho; $x--) {
-            $aItensDropdown[] = $aItensSoltos[array_key_last($aItensSoltos)];
-            unset($aItensSoltos[array_key_last($aItensSoltos)]);
-        }
-    }
-
-    mudarMenus($aItensSoltos, $aItensDropdown, 8); 
+    $aItens = [
+        [
+            'Categorias', 
+            'categorias'
+        ],
+        [
+            'Clientes Demográficos',
+            'clientes_demograficos'
+        ],
+        [
+            'Funcionários',
+            'funcionarios'
+        ],
+        [
+            'Região',
+            'regiao'
+        ],
+        [
+            'Transportadoras',
+            'transportadoras'
+        ]        
+    ];
 
 ?>
 
@@ -57,10 +42,10 @@
                     
                 <?php
 
-                foreach($aItensSoltos as $linha) {
+                foreach($aItens as $linha) {
                     echo '  <li class="nav-item">
-                                <a href="?pg=' . $linha[0] . '" class="nav-link">' . 
-                                    ucwords($linha[0]) . 
+                                <a href="?pg=' . $linha[1] . '" class="nav-link">' . 
+                                    $linha[0] . 
                                 '</a>
                             </li>';
                 }
@@ -69,27 +54,7 @@
                 
                 </ul>
 
-                <ul class="navbar-nav ml-3">
-                    <li class="navbar-item dropdown">
-
-                        <a class="nav-item dropdown-toggle text-light" href="#" id="navDrop" data-toggle="dropdown">
-                            Outras Tabelas
-                        </a>
-
-                        <div class="dropdown-menu">
-                            <?php 
-
-                            foreach ($aItensDropdown as $linha) {
-                                echo '<a class="dropdown-item" href="?pg=' . $linha[0] . '">' .
-                                        ucwords(str_replace('_', ' ', $linha[0])) . '                     
-                                    </a>';
-                            }
-                            
-                            ?>                            
-                                    
-                        </div>
-                    </li>
-                </ul>
+               
             </div>
         </div>
     </nav>
